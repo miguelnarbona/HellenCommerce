221dde26f7f621b88765cf2cfe7f5bf82cd24b1f
miguelnarbona <miguelnarbona@gmail.com>
2026-09-08 14:48:37 -0400

    fix: error en ruta relativa from app.builder.AppBuilder to builder.AppBuilder

--- worker_service/main.py
+++ worker_service/main.py
@@ -18,7 +18,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from contextlib import asynccontextmanager
 # from app.builder.AppBuilder import AppBuilder
-from app.builder.AppBuilder import AppBuilder
+from builder.AppBuilder import AppBuilder
 
 # Importamos las dependencias compartidas (simuladas aquí, deben estar en /app/ en el contenedor)
 system = platform.system()   
