4bacadbdb81713a17fae354844cb58328dca3c2c
miguelnarbona <miguelnarbona@gmail.com>
2026-09-17 00:25:57 -0400

    fix: mistral: logging error

--- mistral_service/main.py
+++ mistral_service/main.py
@@ -125,7 +125,7 @@ async def log_to_logging_service(level: str, msg: str, status_flag="SOLUCIONADO"
             payload = {
                 "timestamp": now.isoformat(),
                 "log_level": level,
-                "service_origin": "venta_service",
+                "service_origin": "mistral_service",
                 "source_file": "main.py",
                 "line_number": line_num,
                 "file_path": __file__,
@@ -134,6 +134,7 @@ async def log_to_logging_service(level: str, msg: str, status_flag="SOLUCIONADO"
                 "proposed_solution": "",
                 "status_flag": status_flag
             }
+            print(f"[DEBUG] Enviando log (venta): {payload}")
             await ws.send(json.dumps(payload))
             await asyncio.sleep(0.01) 
     except Exception as e:
